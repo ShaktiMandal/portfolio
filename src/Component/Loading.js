33a6f9227b32110a2135a1a2fb4110b78478704f
s2304 <34964652+s2304@users.noreply.github.com>
2021-09-30 09:20:49 +0530

    Spelling mistake issue

--- src/Component/Loading.js
+++ src/Component/Loading.js
@@ -5,7 +5,7 @@ import classes from './Loading,module.css'
 const Loading = () => {
     return(
         <div className={`container-fluid vh-100 d-flex align-items-center bg-dark justify-content-center`}>
-            <h1 className={`text-white`}>Loading my porftfolio...</h1>
+            <h1 className={`text-white`}>Loading my portfolio...</h1>
         </div>
 
     )
