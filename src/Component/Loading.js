0bde6aef5029a1b7712c09386bbf56b505818c1a
s2304 <34964652+s2304@users.noreply.github.com>
2021-11-12 15:33:08 +0530

    Added loading animation

--- src/Component/Loading.js
+++ src/Component/Loading.js
@@ -1,11 +1,16 @@
 import React from 'react'
-import classes from './Loading,module.css'
+import classes from './Loading.module.css'
 
 
 const Loading = () => {
     return(
-        <div className={`container-fluid vh-100 d-flex align-items-center bg-dark justify-content-center`}>
-            <h1 className={`text-white`}>Loading my portfolio...</h1>
+        <div className={classes.loadingContainer}>
+            {/* <h1 className={`text-white`}>Loading my portfolio...</h1> */}
+            <div className={classes.loading}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
         </div>
 
     )
